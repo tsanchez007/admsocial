088456387780b62e414b34f0981efd49b80b2d75
tsanchez007 <info.tauliosanchez@gmail.com>
2026-03-10 00:19:48 -0400

    Fix: debug META_APP_ID en login

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,4 +1,12 @@
 export default function handler(req, res) {
+    const appId = process.env.META_APP_ID;
+    const apiVersion = process.env.META_API_VERSION || 'v19.0';
+    const frontendUrl = process.env.FRONTEND_URL;
+    
+    if (!appId) {
+        return res.status(500).json({ error: 'META_APP_ID no configurado', env: Object.keys(process.env).filter(k => k.startsWith('META')) });
+    }
+
     const scope = [
         'pages_manage_posts',
         'pages_read_engagement',
@@ -8,8 +16,8 @@ export default function handler(req, res) {
         'public_profile'
     ].join(',');
 
-    const redirectUri = encodeURIComponent(`${process.env.FRONTEND_URL}/api/auth/callback`);
-    const url = `https://www.facebook.com/${process.env.META_API_VERSION}/dialog/oauth?client_id=${process.env.META_APP_ID}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
+    const redirectUri = encodeURIComponent(`${frontendUrl}/api/auth/callback`);
+    const url = `https://www.facebook.com/${apiVersion}/dialog/oauth?client_id=${appId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
     
     res.redirect(url);
 }
